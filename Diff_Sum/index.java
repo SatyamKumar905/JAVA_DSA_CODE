01805198104cf88fd2ed8c95b27c953056d8011b
satyamkumar905 <satyambhagat905@gmail.com>
2024-12-05 23:58:13 +0530

    Hello World

---
+++ Diff_Sum/index.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+
+public class index {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number :");
+        int x = Scanner.nextInt();
+
+        System.out.println("Enter the second number :");
+        int y = Scanner.nextInt();
+
+        System.out.println("Sum of equal values :");
+        int sum = x + y;
+        System.out.println(sum);
+
+    
+    }
+    
+}
